8691577593fc3219706de3931d551e087b043a61
Jason Gordon <jason.m.gordon@gmail.com>
2017-10-22 17:26:19 -0700

    chore(build): got build working to deploy into drupal module

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -162,46 +162,40 @@ module.exports = {
           // in the main CSS file.
           {
             test: /\.css$/,
-            loader: ExtractTextPlugin.extract(
-              Object.assign(
+            include: paths.appSrc,
+            use: ExtractTextPlugin.extract({
+              fallback: "style-loader",
+              use: [
                 {
-                  fallback: require.resolve("style-loader"),
-                  use: [
-                    {
-                      loader: require.resolve("css-loader"),
-                      options: {
-                        importLoaders: 1,
-                        modules: true,
-                        minimize: true,
-                        sourceMap: shouldUseSourceMap
-                      }
-                    },
-                    {
-                      loader: require.resolve("postcss-loader"),
-                      options: {
-                        // Necessary for external CSS imports to work
-                        // https://github.com/facebookincubator/create-react-app/issues/2677
-                        ident: "postcss",
-                        plugins: () => [
-                          require("postcss-flexbugs-fixes"),
-                          autoprefixer({
-                            browsers: [
-                              ">1%",
-                              "last 4 versions",
-                              "Firefox ESR",
-                              "not ie < 9" // React doesn't support IE8 anyway
-                            ],
-                            flexbox: "no-2009"
-                          })
-                        ]
-                      }
-                    }
-                  ]
+                  loader: "css-loader",
+                  options: {
+                    modules: true,
+                    localIdentName: "[name]__[local]___[hash:base64:5]"
+                  }
                 },
-                extractTextPluginOptions
-              )
-            )
-            // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
+                "postcss-loader"
+              ]
+            })
+          },
+          // Process Sass
+          {
+            test: /\.scss$/,
+            include: paths.appSrc,
+            use: ExtractTextPlugin.extract({
+              fallback: "style-loader",
+              use: [
+                {
+                  loader: "css-loader",
+                  options: {
+                    modules: true,
+                    sourceMap: false,
+                    importLoaders: 2,
+                    localIdentName: "[name]__[local]___[hash:base64:5]"
+                  }
+                },
+                "sass-loader"
+              ]
+            })
           },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
