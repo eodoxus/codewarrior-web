864bfd98f70cd59df701dba3d50aa06284066414
Jason Gordon <jason.m.gordon@gmail.com>
2017-11-02 21:43:51 -0700

    fix(deploy): remove sensitive information from deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,15 +5,14 @@ echo "Pushing to git..."
 git push
 yarn build
 echo "Creating tarball..."
-tar cf public.tgz -C $REACT_APP_BUILD_DESTINATION .
+tar cf deploy.tgz -C $REACT_APP_BUILD_DESTINATION .
 echo "Deploying to remote..."
-scp public.tgz www-data@jaygordo.com:$REACT_APP_SERVER_PATH
-ssh www-data@jaygordo.com "\
+scp deploy.tgz $REACT_APP_DEPLOY_USER@$REACT_APP_REMOTE_HOST:$REACT_APP_SERVER_PATH
+ssh $REACT_APP_DEPLOY_USER@$REACT_APP_REMOTE_HOST "\
   cd $REACT_APP_SERVER_PATH \
-  && rm -rf public \
-  && tar -xf public.tgz \
-  && rm -f public.tgz \
+  && tar -xf deploy.tgz \
+  && rm -f deploy.tgz \
   && cp index.html ./page-codewarrior.tpl.php"
-rm -f public.tgz
+rm -f deploy.tgz
 echo "Clearing cache..."
-curl -Is http://jaygordo.com/clearcache | head -1
+curl -Is http://$REACT_APP_REMOTE_HOST/clearcache | head -1
