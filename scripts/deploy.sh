266f1ae0c6a78cd116a5f37c01a56658bfb125b3
Jason Gordon <jason.m.gordon@gmail.com>
2017-11-02 21:31:28 -0700

    chore(deploy): rewrote deploy script to run build locally then tar, upload and unpack remotely

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+source .env.local
 
-cd /var/www/jaygordo.com/sites/all/modules/custom/codewarrior/react-app
-git reset --hard HEAD
-git pull
-yarn install
+echo "Pushing to git..."
+git push
 yarn build
+echo "Creating tarball..."
+tar cf public.tgz -C $REACT_APP_BUILD_DESTINATION .
+echo "Deploying to remote..."
+scp public.tgz www-data@jaygordo.com:$REACT_APP_SERVER_PATH
+ssh www-data@jaygordo.com "\
+  cd $REACT_APP_SERVER_PATH \
+  && rm -rf public \
+  && tar -xf public.tgz \
+  && rm -f public.tgz \
+  && cp index.html ./page-codewarrior.tpl.php"
+rm -f public.tgz
+echo "Clearing cache..."
+curl -Is http://jaygordo.com/clearcache | head -1
