ec2d759fe806d1e1c707b79e5edd2006e548149d
Jason Gordon <jason.m.gordon@gmail.com>
2018-01-04 18:21:23 -0800

    test(SceneTransitioner): backfilled tests for SceneTransitioner class

---
+++ src/game/engine/SceneTransitioner.test.js
@@ -0,0 +1,90 @@
+import Graphics from "./Graphics";
+import Hero from "../entities/hero/Hero";
+import NoGraphicsComponent from "../entities/components/graphics/NoGraphicsComponent";
+import Scene from "./Scene";
+import SceneTransitioner from "./SceneTransitioner";
+import Size from "./Size";
+import Tile from "./map/Tile";
+import Vector from "./Vector";
+import TransitioningState from "../entities/hero/states/TransitioningState";
+
+jest.useFakeTimers();
+jest.mock("./Graphics");
+jest.mock("./Scene");
+
+Graphics.getScale.mockReturnValue(0.75);
+const mockHero = new Hero();
+mockHero.setProperty("width", "24");
+mockHero.setProperty("height", "32");
+mockHero.setGraphics(new NoGraphicsComponent(mockHero));
+mockHero.render = jest.fn();
+
+function createTransitioner(orientation = { x: "0", y: "1" }) {
+  mockHero.setPosition(new Vector(100, 100));
+  const transitionTile = new Tile(new Vector(), new Size(10, 10));
+  transitionTile.setProperties({
+    orientationX: orientation.x,
+    orientationY: orientation.y
+  });
+  const fromScene = new Scene("fromScene", mockHero);
+  const toScene = new Scene("toScene", mockHero);
+  return new SceneTransitioner(
+    mockHero,
+    fromScene,
+    toScene,
+    transitionTile,
+    new Size(300, 300)
+  );
+}
+
+function finishTransition(transitioner) {
+  while (!transitioner.isDone) {
+    jest.runOnlyPendingTimers();
+  }
+}
+
+describe("SceneTransitioner", () => {
+  it("scales scene size to from dom to canvas scale", () => {
+    createTransitioner();
+    expect(SceneTransitioner.getSceneSize()).toEqual(new Size(225, 225));
+  });
+
+  describe("animate", () => {
+    it("puts Hero into transitioning state and moves scenes through transition, constraining Hero on scene", () => {
+      let transitioner = createTransitioner();
+      transitioner.animate();
+      expect(mockHero.getState() instanceof TransitioningState).toBe(true);
+      finishTransition(transitioner);
+      expect(transitioner.to.position).toEqual(new Vector(0, -1));
+      expect(transitioner.from.position).toEqual(new Vector(0, -225));
+      expect(mockHero.getPosition()).toEqual(new Vector(88, 12.88));
+    });
+
+    it("transitions scenes down", async () => {
+      let transitioner = createTransitioner({ x: "0", y: "-1" });
+      transitioner.animate();
+      finishTransition(transitioner);
+      expect(transitioner.to.position).toEqual(new Vector(0, 1));
+      expect(transitioner.from.position).toEqual(new Vector(0, 225));
+      expect(mockHero.getPosition()).toEqual(new Vector(88, 180.52));
+    });
+
+    it("transitions scenes left", async () => {
+      let transitioner = createTransitioner({ x: "-1", y: "0" });
+      transitioner.animate();
+      finishTransition(transitioner);
+      expect(transitioner.to.position).toEqual(new Vector(1, 0));
+      expect(transitioner.from.position).toEqual(new Vector(225, 0));
+      expect(mockHero.getPosition()).toEqual(new Vector(189.6, 84));
+    });
+
+    it("transitions scenes right", async () => {
+      let transitioner = createTransitioner({ x: "1", y: "0" });
+      transitioner.animate();
+      finishTransition(transitioner);
+      expect(transitioner.to.position).toEqual(new Vector(-1, 0));
+      expect(transitioner.from.position).toEqual(new Vector(-225, 0));
+      expect(mockHero.getPosition()).toEqual(new Vector(11.8, 84));
+    });
+  });
+});
--- src/game/engine/__mocks__/Scene.js
+++ src/game/engine/__mocks__/Scene.js
@@ -1,3 +1,7 @@
+import Texture from "../Texture";
+import Vector from "../Vector";
+import Size from "../Size";
+
 export default class Scene {
   constructor(name, hero) {
     this.name = name;
@@ -9,9 +13,16 @@ export default class Scene {
   init() {
     return Promise.resolve();
   }
+  addEntity = jest.fn();
   onClick = jest.fn();
   render = jest.fn();
   update = jest.fn();
   unload = jest.fn();
   shouldShowBorder = jest.fn();
+  removeEntity = jest.fn();
+  renderToTexture = jest
+    .fn()
+    .mockReturnValue(
+      new Texture("testTexture", new Vector(), new Size(10, 10))
+    );
 }
